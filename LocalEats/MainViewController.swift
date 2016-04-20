2a45392d89823dc9d2c4bcd87fa8d592fa399049
Kevin Lowe <lowekevin123@gmail.com>
2016-04-19 21:35:23 -0700

    Able to prompt user for current location

--- LocalEats/MainViewController.swift
+++ LocalEats/MainViewController.swift
@@ -10,17 +10,37 @@ import UIKit
 import CoreLocation
 
 class MainViewController: UIViewController, CLLocationManagerDelegate {
+    let locationManager = CLLocationManager()
+    var locationForYelp: CLLocation!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        if CLLocationManager.authorizationStatus() == .NotDetermined {
+            locationManager.requestWhenInUseAuthorization()
+        }
+        locationManager.requestLocation()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
+        print("Error while updating location " + error.localizedDescription)
+    }
 
-
+    func locationManager(manager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+        if status != .AuthorizedWhenInUse {
+            locationManager.requestWhenInUseAuthorization()
+        }
+    }
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        locationForYelp = locations[locations.count - 1]
+    }
 }
 
