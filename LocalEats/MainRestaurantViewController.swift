b259d576ac2da85f3f10909c35caa8efdc8b4a84
Kevin Lowe <lowekevin123@gmail.com>
2016-04-27 14:14:54 -0700

    Complete cleanse of Carthage and Pods

--- LocalEats/MainRestaurantViewController.swift
+++ LocalEats/MainRestaurantViewController.swift
@@ -139,7 +139,6 @@ class MainRestaurantViewController: UIViewController, CLLocationManagerDelegate 
     func loadLaunchElements() {
         let screen = UIScreen.mainScreen().bounds.size
         let viewWidth = screen.width - 50
-        let viewHeight = screen.height - (screen.height * 4 / 10) - 15
         let viewY = (self.navigationController?.navigationBar.frame.size.height)! + UIApplication.sharedApplication().statusBarFrame.size.height + 25
         loadingView = UIImageView(frame: CGRectMake(25, viewY, viewWidth, viewWidth))
         let yelpGif = UIImage.gifWithName("yelp_star")
